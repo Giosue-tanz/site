c1b848c19a136ff69860bd3e306eb8d0e1b0972c
Giosue-tanz <giosue.tanz@gmail.com>
2024-08-10 22:26:15 +0200

    Aggiorna file del progetto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-//import Hero from './components/hero/Hero';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Abhome from './components/Abhome/Abhome';
-import Cards from './components/card/Cards'; 
-import List from './components/List/List'; 
+import Cards from './components/card/Cards';
+import List from './components/List/List';
 import Footer from './components/footer/Footer';
 import AboutMe from './components/aboutme/aboutme';
 
 function App() {
+  const basename = process.env.NODE_ENV === 'production' ? '/SITE' : '';
+
   return (
-    <>
+    <Router basename={basename}>
       <Navbar />
-      <AboutMe />
-      <Abhome />
-      <List /> 
-      <Cards />
+      <Routes>
+        <Route path="/" element={<Abhome />} />
+        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/cards" element={<Cards />} />
+        <Route path="/list" element={<List />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
 export default App;
-
