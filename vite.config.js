0338fb4269d5f1ad3d86788e449f0d3d65c9b8be
Giosuè Aiello <giosue.tanz@gmail.com>
2025-05-24 03:02:10 +0200

    Aggiornamenti per deploy con Vite e GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/site/', // Sostituisci con il nome del tuo repository GitHub
+  base: '/site/', // Sostituisci con il nome del tuo repository
   server: {
     port: 3000,
   },
